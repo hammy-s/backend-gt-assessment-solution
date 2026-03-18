3a47a8df5b5dcafb12f7684911d9573978221706
hammy-s <bellohammed012@gmail.com>
2026-03-18 15:35:17 +0100

    Update tasks.js

--- task-2-api-implementation/src/routes/tasks.js
+++ task-2-api-implementation/src/routes/tasks.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const extractUserId = require('../middleware/auth');
+const {
+  createTask,
+  getTasks,
+  updateTaskDetails,
+  updateTaskStatus,
+  unassignTask,
+  deleteTask
+} = require('../controllers/taskController');
 
+router.use(extractUserId);
+
+router.post('/', createTask);
+router.get('/', getTasks);
+router.patch('/:id', updateTaskDetails);
+router.patch('/:id/status', updateTaskStatus);
+router.patch('/:id/unassign', unassignTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
