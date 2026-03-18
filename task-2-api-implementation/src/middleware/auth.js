57274039659045bb22e64d275c33aa5377415905
hammy-s <bellohammed012@gmail.com>
2026-03-18 15:36:36 +0100

    Update auth.js

--- task-2-api-implementation/src/middleware/auth.js
+++ task-2-api-implementation/src/middleware/auth.js
@@ -1,1 +1,10 @@
+const extractUserId = (req, res, next) => {
+  const userId = req.headers['x-user-id'];
+  if (!userId) {
+    return res.status(400).json({ error: 'Missing x-user-id header' });
+  }
+  req.userId = parseInt(userId, 10);
+  next();
+};
 
+module.exports = extractUserId;
